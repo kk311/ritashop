931fd4167d9762376d3284769f4452a3a1fdddad
kk311 <hiep3010@hotmail.com>
2014-04-27 23:19:52 -0700

    Create reveal.php

---
+++ reveal.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Thanks For Liking Our Page!</title>
+</head>
+
+<body>
+<style type='text/css']]>
+#wrapper {
+margin:0 auto; border:0; padding:0;
+/* Change the dimensions of the image to fit the size of your desired image */
+width:520px;
+height:500px;
+position:relative;
+}
+</style>
+<div id="wrapper">
+  <div style='width:520px;'>
+     <!-- This image will be displayed to fans -->
+     <img src='https://s3.amazonaws.com/cdn.16wells.com/facebook/reveal-after.png' /> 
+     <!-- You can also add or replace with any HTML that you want displayed to fans -->
+  </div>
+</div>
+</body>
+</html>
